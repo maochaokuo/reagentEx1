fbf596210b279dd38518d1f31e0274add6888562
Thomas Mao <tm1813@gmail.com>
2021-05-30 15:50:53 +0800

    final version

--- src/simpletodo/core.cljs
+++ src/simpletodo/core.cljs
@@ -1,45 +1,83 @@
 (ns simpletodo.core
-    (:require
-      [reagent.core :as r]
-      [reagent.dom :as d]))
 
-;; -------------------------
-;; Views
+  (:require
+
+   [reagent.core :as r]
+
+   [reagent.dom :as d]))
 
 (def todos (r/atom
-            [{:desc "Fry the garlic" :color "green"}
-             {:desc "Boil the pasta" :color "red"}]))
+
+            [{:desc "Fry the garlic" :completed true}
+
+             {:desc "Boil the pasta" :completed false}]))
 
 (defn todo-form []
-    [new-item (r/atom "")]
-  [:form {on-submit (fn [e]
-                      (.preventDefault e)
-                      ())}
-    [:input {:type "text" 
-             :value @new-item
-             :placeholder "Add a new item"
-             :on-change (fn [e
-                          (reset! new-item (.-value (.-target e))))}]])$ ;  }]])
+
+  (let [new-item (r/atom "") new-item-completed (r/atom false)]
+
+    (fn []
+
+      [:form {:on-submit (fn [e]
+
+                           (.preventDefault e)
+
+                           (swap! todos conj {:completed @new-item-completed :desc @new-item})
+
+                           (reset! new-item "")
+
+                           (reset! new-item-completed false))}
+
+       [:input {:type "checkbox" :checked @new-item-completed
+
+                :on-change #(reset! new-item-completed (-> % .-target .-checked))}]
+
+       [:input {:type "text"
+
+                :value @new-item
+
+                :placeholder "Add a new item"
+
+                :on-change (fn [e]
+
+                             (reset! new-item (.-value (.-target e))))}]])))
 
 (defn todo-item [todo]
-  [:li {:style {:color (:color todo)}} (:desc todo)])
+
+  [:li {:style {:color (if (:completed todo) "green" "red")}} (:desc todo)])
+
+(defn todo-list []
+
+  [:ul
+
+   (for [todo @todos]
+
+     (todo-item todo))])
 
 (defn home-page []
-  [:div 
+
+  [:div
+
    [:h2 "Lists keep it simple"]
+
    [:p "Add a new item below:"]
-   [:ul 
-    (for [todo @todos]
-      (todo-item todo))]])
-;    [:li {:style {:color "red"}} "Boil the pasta"]
-;    [:li {:style {:color "green"}}"Grind the basil"]
-;    [:li "Fry the garlic"]]])
+
+   [todo-form]
+
+   [todo-list]])
+
+
 
 ;; -------------------------
+
 ;; Initialize app
 
+
 (defn mount-root []
+
   (d/render [home-page] (.getElementById js/document "app")))
 
 (defn ^:export init! []
+
   (mount-root))
+
